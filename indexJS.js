472c20f128df187ada64c660a0de170b0e96da95
AJ Fischer <etherealNekryx@users.noreply.github.com>
2022-02-13 19:37:23 -0600

    Updated JS to hopefully work better

--- indexJS.js
+++ indexJS.js
@@ -1,5 +1,26 @@
-var valR = document.getElementById("Rinput").innerHTML;
-function sliderValueR(valR) {
-	document.getElementById("Rinput").innerHTML = valR;
+var colorPicker;
+var defaultColor = "#0000ff";
+
+window.addEventListener("load", startup, false);
+
+function startup() {
+  colorPicker = document.querySelector("#colorPicker");
+  colorPicker.value = defaultColor;
+
+  colorPicker.addEventListener("input", updateFirst, false);
+  colorPicker.addEventListener("change", watchColorPicker, false);
 }
-document.getElementById("Rval").value = valR;
+
+function updateFirst(event) {
+  var p = document.querySelector("p");
+  
+  if (p) {
+    p.style.color = event.target.value;
+  }
+}
+
+function watchColorPicker(event) {
+  document.querySelectorAll("p").forEach(function(p) {
+    p.style.color = event.target.value;
+  });
+}
