6f84b37008ead9b357641e9a5b0b9df52a9864b1
AJ Fischer <etherealNekryx@users.noreply.github.com>
2022-02-14 22:50:32 -0600

    [Finishes #181207499] Set up JS to calculate BM values and display them next to the RGB text boxes

--- indexJS.js
+++ indexJS.js
@@ -3,18 +3,17 @@ function calculate() {
   var R = document.getElementById("Rinput").value;
   var G = document.getElementById("Ginput").value;
   var B = document.getElementById("Binput").value;
-  
-  var RGB = R + G + B;
-  
-  var r = (1 / RGB) * R;
-  var g = (1 / RGB) * G;
-  var b = (1 / RGB) * B;
-  
   var Rval = document.getElementById("Rval");
   var Gval = document.getElementById("Gval");
   var Bval = document.getElementById("Bval");
   
-  Rval.innHTML = r;
+  var RGB = +R + +G + +B;
+  
+  var r = ((1 / RGB) * R).toFixed(3);
+  var g = ((1 / RGB) * G).toFixed(3);
+  var b = ((1 / RGB) * B).toFixed(3);
+  
+  Rval.innerHTML = r;
   Gval.innerHTML = g;
   Bval.innerHTML = b;
   
